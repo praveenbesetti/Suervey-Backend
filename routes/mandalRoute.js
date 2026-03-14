48720e1ed6a477ca0e9d44f764175d809f523f4c
praveenbesetti <praveenbesetti0@gmail.com>
2026-03-14 11:14:32 +0530

    new deployee

--- routes/mandalRoute.js
+++ routes/mandalRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { getAgentDetailsByDistrict, getMandals,updateMandalAgent } from '../controllers/mandalController.js';
 
 const router = express.Router();
-router.get('/:districtId', getMandals);
 router.get('/agent/:districtId', getAgentDetailsByDistrict);
+router.get('/:districtId', getMandals);
 router.put('/:mandalId/agent', updateMandalAgent);
 export default router;
